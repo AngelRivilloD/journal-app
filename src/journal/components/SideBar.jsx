f75756363c8cdafb655d3a858485cbe292ae7f12
AngelRivilloD <antorivillo@gmail.com>
2023-04-03 10:08:23 +0200

    first commit

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,48 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Drawer, Box, Toolbar, Typography, Divider, List, ListItem, ListItemButton, ListItemIcon, Grid, ListItemText } from "@mui/material"
+
+
+export const SideBar = ({drawerWidth}) => {
+  return (
+    <Box
+        component='nav'
+        sx={{ width: { sm: drawerWidth}, flexShrink: { sm: 0 }}}
+    >
+        <Drawer 
+            variant="permanent" 
+            open="true" 
+            sx={{
+                display: { xs: 'block'},
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+        >
+            <Toolbar>
+                <Typography variant='h6' noWrap component='div'>
+                    Angel Rivillo
+                </Typography>
+            </Toolbar>
+            <Divider/>
+
+            <List>
+                {   
+                    ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
+                        <ListItem key={ text } disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot/>
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={text}/>
+                                    <ListItemText secondary={'Bla blablabla jajsjs'}/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+        </Drawer>
+
+    </Box>
+  )
+}
